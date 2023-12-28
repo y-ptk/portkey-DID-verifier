b92673c61e9a8d2918e3ce9756f1dfabd6feb4a0
nill66 <sofiat.nill@portkey.finance>
2023-12-28 14:07:08 +0800

    feat: telegram login

--- src/CAVerifierServer.Grains/Options/JwtTokenOptions.cs
+++ src/CAVerifierServer.Grains/Options/JwtTokenOptions.cs
@@ -5,5 +5,5 @@ public class JwtTokenOptions
     public string PrivateKey { get; set; }
     public string Issuer { get; set; }
     public IEnumerable<string> Audiences { get; set; }
-    public int expire { get; set; }
+    public int Expire { get; set; }
 }
